6bed8c4bbe548c67bac8975198bb8e40356cc699
Satvik <satvik.km@kapturecrm.com>
2024-06-21 11:22:47 +0530

    changes

--- databaseConnection.js
+++ databaseConnection.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
     host: '34.123.234.76',
-    user: 'root',
+    user: 'mysql_user',
     password: 'MySql@2024',
-    database: 'mysql_user',
+    database: 'code_brew',
     waitForConnections: true,
     connectionLimit: 100, // Adjust the connection limit as needed
     queueLimit: 0,
--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const usersRoute = require('./routes/usersRoutes');
 const apiHandler = require('./routes/apiHandlerRoutes');
 
 const app = express();
-const port = 3001;
+const port = 8080;
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/', usersRoute);
+app.use('/api', usersRoute);
 
-app.use('/api-handler', apiHandler);
+app.use('/api/api-handler', apiHandler);
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
